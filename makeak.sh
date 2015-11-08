cb48aea9cc1e7e9ba820b10643021ca6ed553a56
fermasia <fermasia@hotmail.com>
2015-11-08 15:35:24 -0300

    adapt build script for CM13

--- makeak.sh
+++ makeak.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-TOOLCHAIN="/home/nando/dev/toolchains/uber49/bin"
+TOOLCHAIN="/home/nando/dev/toolchains/gcc48/bin"
 echo "Cleaning old files"
-rm -f ../AnykernelOMNI/dtb
-rm -f ../AnykernelOMNI/zImage
+rm -f ../AnykernelCYANO/dtb
+rm -f ../AnykernelCYANO/zImage
 echo "Making kernel"
 DATE_START=$(date +"%s")
 
@@ -10,7 +10,7 @@ make clean && make mrproper
 
 export ARCH=arm
 export SUBARCH=arm
-make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- msm8974_find7op_defconfig
+make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- cyanogenmod_bacon_defconfig
 make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- -j2
 echo "End of compiling kernel!"
 
@@ -19,9 +19,9 @@ echo
 DIFF=$(($DATE_END - $DATE_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 
-../ramdisk_one_plus_one/dtbToolCM -2 -o ../AnykernelOMNI/dtb -s 2048 -p ../omni/scripts/dtc/ ../omni/arch/arm/boot/
-cp arch/arm/boot/zImage ../AnykernelOMNI/zImage
-cd ../AnykernelOMNI/
-zipfile="DONKEY-V"$1".zip"
+../ramdisk_one_plus_one/dtbToolCM -2 -o ../AnykernelCYANO/dtb -s 2048 -p ../cyano/scripts/dtc/ ../cyano/arch/arm/boot/
+cp arch/arm/boot/zImage ../AnykernelCYANO/zImage
+cd ../AnykernelCYANO/
+zipfile="CYAN-v"$1".zip"
 zip -r -9 $zipfile *
-mv DONKEY-V*.zip ../OUT/
+mv CYAN-v*.zip ../OUT/
